2740a944babe5d55284b02f56262d9cbd699e557
Fahad Ahmed <fahadsimari111@gmail.com>
2024-02-24 09:37:11 +0500

    first commit

---
+++ classes.dart
@@ -0,0 +1,39 @@
+// void main() {
+//   Persson objectperson = Persson();
+//   objectperson.isEating();
+// }
+
+// class Persson {
+//   String name = "";
+//   isEating() {}
+// }
+
+// class Teacher extends Persson {}
+
+// class Student {}
+// void main() {
+//   CarA obj = CarA();
+//   obj.speed();
+// }
+
+// class car {
+//   autoFeature() {
+//     print("power window");
+//     print("power window");
+//     print("power window");
+//     print("power window");
+//   }
+// }
+
+// class CarA extends car {
+//   @override
+//   speed() {
+//     super.autoFeature();
+//     print("Body Change");
+//   }
+// }
+
+class A {
+  String name = "bilal";
+  String sec = "a";
+}
---
+++ main.dart
@@ -0,0 +1,8 @@
+import 'classes.dart';
+void main(){
+  
+}
+
+class B extends A {
+  String rollno = "66";
+}
